f16cbc26f41291764adf4272e355ec9210c33e30
DESKTOP-7UST7UV\fcmaug <fcmaug@gmail.com>
2017-03-28 00:57:18 -0400

    .

---
+++ Assets/Scripts/ChunkSelection.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class ArrayExtensions
+{
+    public static T GetRandom<T>(this T[] array)
+    {
+        return array[Random.Range(0, array.Length)];
+    }
+}
+
+public class ChunkSelection : MonoBehaviour
+{
+
+    [Header("Prefab references")]
+    public NestedPrefab[] chunks;
+
+    // Componente do objeto escolhido aleatoriamente
+    public NestedPrefab lastSelectedChunk;
+    public ScrollingObject currentScrollObject;
+
+    public void SelectChunk()
+    {
+        if (currentScrollObject == null)
+            currentScrollObject = GetComponent<ScrollingObject>();
+
+        lastSelectedChunk = chunks.GetRandom();
+        // Pega o tamanho do chunk atual
+        currentScrollObject.size =
+            lastSelectedChunk.GetComponent<ScrollingObject>().size;
+
+    }
+
+    public Transform GenerateChunk()
+    {
+        return Instantiate(lastSelectedChunk.transform) as Transform;
+    }
+}
---
+++ Assets/Scripts/ScrollingObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ScrollingObject : MonoBehaviour
+{
+
+    public Vector3 scrollVelocity;
+    public Vector3 size;
+
+    public Bounds bounds;
+
+    public virtual void Update()
+    {
+        transform.position += scrollVelocity * Time.deltaTime;
+    }
+
+    [ContextMenu("Update Size From Children")]
+    public void UpdateSizeFromChildren()
+    {
+        bounds = new Bounds(transform.position, Vector3.one);
+        Renderer[] renderers = GetComponentsInChildren<Renderer>();
+        foreach (Renderer renderer in renderers)
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        size = bounds.size;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(1, 0, 0, 0.5f);
+        Gizmos.DrawWireCube(bounds.center, bounds.size);
+    }
+}
